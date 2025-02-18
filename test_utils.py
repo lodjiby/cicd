9f055b960d3b1111bf7c3ba52be017b5da10f5a8
DJIBY <macbook@OUSMANE-LO.local>
2025-02-18 22:16:25 +0000

    Set up simple Python project with CI workflow

--- test_utils.py
+++ test_utils.py
@@ -1,11 +1,17 @@
 import unittest
 
+# Sample function to test
+def add(a, b):
+    return a + b
 
+# Test case class
+class TestUtils(unittest.TestCase):
+    
+    def test_add(self):
+        result = add(2, 3)
+        self.assertEqual(result, 5)
 
-class TestUtilsMethods (unittest.TestCase):
+    def test_add_negative(self):
+        result = add(-1, 1)
+        self.assertEqual(result, 0)
 
-    def test_playfair (self):
-        self.assertTrue (True)
-
-if __name__ == '__main__':
-    unittest.main()
