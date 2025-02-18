8390070a65b91f333004ab7ea12a813b46082229
DJIBY <macbook@OUSMANE-LO.local>
2025-02-18 21:42:11 +0000

    first commit

---
+++ test_utils.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+
+class TestUtilsMethods (unittest.TestCase):
+
+    def test_playfair (self):
+        self.assertTrue (True)
+
+if __name__ == '__main__':
+    unittest.main()
